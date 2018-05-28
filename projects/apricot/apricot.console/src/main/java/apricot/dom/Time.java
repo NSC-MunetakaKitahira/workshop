4d4bd72e4413aa46734f2752890b720f9a685796
NTS\yuta_sano <yuta_sano@nittsusystem.co.jp>
2018-05-28 15:34:55 +0900

    （TimePeriod内で時間どうしの比較や演算が必要になったので,機能を追加しました.）

--- projects/apricot/apricot.console/src/main/java/apricot/dom/Time.java
+++ projects/apricot/apricot.console/src/main/java/apricot/dom/Time.java
@@ -6,6 +6,7 @@ package apricot.dom;
  */
 public class Time{
 	private static final String timeFormat = "%tH:%tM";
+	private static final int hourDivide = 100;
 	int timeOfInt;
 	
 	
@@ -43,15 +44,76 @@ public class Time{
 	 * @return timeの時間部分
 	 */
 	public int hours() {
-		return timeOfInt / 100;
+		return timeOfInt / hourDivide;
 	}
 	/**
 	 * 現在の分：minutesを返します.
 	 * @return timeの分部分
 	 */
 	public int minutes() {
-		return timeOfInt % 100;
+		return timeOfInt % hourDivide;
 	}
+	
+	
+	/**
+	 * 時間の引き算（e.g 12:00-8:30 = 3:30）
+	 * @param target
+	 * @return 2つの時間の間に生じる
+	 */
+	public Time sub(Time target) {
+		if(this.lt(target))
+			return new Time(0);
+		int hours = this.hours() - target.hours();
+		int minutes = this.minutes() - target.minutes();
+		if(minutes < 0) {
+			hours -= 1;
+			minutes = 60 - minutes;
+		}
+		return new Time(hours*hourDivide + minutes); 
+	}
+	/**
+	 * @param target
+	 * @return this == target
+	 */
+	public boolean eq(Time target) {
+		return this.toInt() == target.toInt(); 
+	}
+	/**
+	 * @param target
+	 * @return this != target
+	 */
+	public boolean ne(Time target) {
+		return !(this.eq(target)); 
+	}
+	/**
+	 * @param target
+	 * @return this > target
+	 */
+	public boolean gt(Time target) {
+		return this.toInt() > target.toInt(); 
+	}
+	/**
+	 * @param target
+	 * @return this < target
+	 */
+	public boolean lt(Time target) {
+		return this.toInt() < target.toInt(); 		
+	}
+	/**
+	 * @param target
+	 * @return this >= target
+	 */
+	public boolean ge(Time target) {
+		return this.gt(target) || this.eq(target); 
+	}
+	/**
+	 * @param target
+	 * @return this <= target
+	 */
+	public boolean le(Time target) {
+		return this.lt(target) || this.eq(target); 
+	}
+	
 	
 	/**
 	 * 文字列形式の時間を整数形式の時間に変換します.
@@ -60,7 +122,7 @@ public class Time{
 	 */
 	public static int parseTimeFrom(String timeOfString) {
 		Time time = new Time(timeOfString);
-		return time.hours() * 60 + time.minutes();
+		return time.hours() * hourDivide + time.minutes();
 	}
 
 	/**
