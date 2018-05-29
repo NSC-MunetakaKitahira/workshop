e230857965f7d44c2cdf2b3ef536f00c3e3d72bc
NTS\yuta_sano <yuta_sano@nittsusystem.co.jp>
2018-05-29 09:46:15 +0900

    （作成したクラスをテストするために必要な機能を揃えました.テストが通れば,テストに問題がなければWorkShiftと置き換えることができるはずです.）

---
+++ projects/apricot/apricot.console/src/main/java/apricot/dom/TimeTable.java
@@ -0,0 +1,99 @@
+package apricot.dom;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TimeTable {
+	private TimePeriod worktime;
+	private List<TimePeriod> overtimeList = new ArrayList<TimePeriod>();
+	private List<TimePeriod> breaktimeList = new ArrayList<TimePeriod>();
+
+	public void setWorktime(String startTimeOfString, String endTimeOfString) {
+		this.worktime = new TimePeriod(startTimeOfString, endTimeOfString);
+	}
+
+	public void addOvertime(String startTimeOfString, String endTimeOfString) {
+		this.overtimeList.add(new TimePeriod(startTimeOfString, endTimeOfString));
+	}
+
+	public void addBreakime(String startTimeOfString, String endTimeOfString) {
+		this.breaktimeList.add(new TimePeriod(startTimeOfString, endTimeOfString));
+	}
+	
+	private TimePeriod getActualWorktimeFrom(TimePeriod target) {
+		return this.worktime.getDuplicationWith(target);
+	}
+	private List<TimePeriod> getActualOvertimeFrom(TimePeriod target){
+		return TimeTable.superimposesTargetOnAllListItem(target,overtimeList);
+	}
+	private List<TimePeriod> getActualBreaktimeFrom(TimePeriod target){
+		return TimeTable.superimposesTargetOnAllListItem(target,breaktimeList);
+	}
+	private static List<TimePeriod> superimposesTargetOnAllListItem (TimePeriod target,List<TimePeriod> thisList){
+		List<TimePeriod> duplicationList = new ArrayList<TimePeriod>();
+		for (TimePeriod timePeriod : thisList) {
+			TimePeriod duplication = timePeriod.getDuplicationWith(target);
+			if(duplication.temporalLength().toInt() != 0)
+				duplicationList.add(duplication);
+		}
+		return duplicationList;
+	}
+	private static List<TimePeriod> splitTargetOnAllListItem (TimePeriod target,List<TimePeriod> thisList){
+		List<TimePeriod> splitList = new ArrayList<TimePeriod>();
+		for (TimePeriod timePeriod : thisList) {
+			TimePeriod[] subtraction = timePeriod.getSubtractionWith(target);
+			if(subtraction.length > 0) {
+				splitList.addAll(Arrays.asList(subtraction));
+				target = subtraction[ subtraction.length - 1 ];
+			}
+		}
+		return splitList;
+	}
+	
+	public void calculate(TimePeriod timeStamp) {
+		
+		// 実就業時間: 出勤～退勤と、始業～終業との重複範囲
+		TimePeriod actualWorktime = this.getActualWorktimeFrom(timeStamp);
+		
+		// 実残業時間帯: 出勤～退勤と、各残業時間帯との重複
+		List<TimePeriod> actualOverworktimeList = this.getActualOvertimeFrom(timeStamp);
+		
+		// 実休憩時間帯: 出勤～退勤と、各休憩時間帯との重複
+		List<TimePeriod> actualBreaktimeList = this.getActualBreaktimeFrom(timeStamp);
+		
+		// 実就業時間帯から実休憩時間帯に重複している部分を除外
+		List<TimePeriod> actualWorktimeListWithoutBreaktime = TimeTable.splitTargetOnAllListItem(actualWorktime, actualBreaktimeList);
+		
+		// 実残業時間帯から実休憩時間帯に重複している部分を除外
+		List<TimePeriod> actualOvertimeListWithoutBreaktime = new ArrayList<TimePeriod>();
+		for (TimePeriod overtime : actualOverworktimeList) {
+			actualOvertimeListWithoutBreaktime.addAll(TimeTable.splitTargetOnAllListItem(overtime, actualBreaktimeList));
+		}
+		
+		System.out.println("就業時間");
+		int sumWorktime = 0;
+		for (TimePeriod worktimeWithoutBreaktime : actualWorktimeListWithoutBreaktime) {
+			System.out.println(worktimeWithoutBreaktime.toString());
+			sumWorktime = worktimeWithoutBreaktime.temporalLength().minutes();
+		}
+		System.out.println("合計: " + Time.parseTimeFrom(sumWorktime));
+		
+		System.out.println("残業時間");
+		int sumOvertime = 0;
+		for (TimePeriod overtimeWithoutBreaktime : actualOvertimeListWithoutBreaktime) {
+			System.out.println(overtimeWithoutBreaktime.toString());
+			sumOvertime = overtimeWithoutBreaktime.temporalLength().minutes();
+		}
+		System.out.println("合計: " + Time.parseTimeFrom(sumOvertime));
+				
+		System.out.println("休憩時間");
+		int sumBreaktime = 0;
+		for (TimePeriod actualBreaktime : actualBreaktimeList) {
+			System.out.println(actualBreaktime.toString());
+			sumBreaktime = actualBreaktime.temporalLength().minutes();
+		}
+		System.out.println("合計: " + Time.parseTimeFrom(sumBreaktime));
+	}
+
+}
