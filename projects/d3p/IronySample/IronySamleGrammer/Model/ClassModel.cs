300e50f1df3a2c42d584f4d00c380bd996e65f00
m_kitahira <m_kitahira@nittsusystem.co.jp>
2020-12-05 12:54:22 +0900

    D3P sample

---
+++ projects/d3p/IronySample/IronySamleGrammer/Model/ClassModel.cs
@@ -0,0 +1,23 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronySamleGrammer.Model
+{
+    public class ClassModel
+    {
+        public string Resp { get; }
+
+        public ClassModel(ParseTreeNode classBlock)
+        {
+            var classDefine = classBlock.ChildNodes[0];
+            string stereoTypeText = classDefine.ChildNodes[0].ChildNodes[0].Token.ValueString;
+
+
+            Resp = "k";
+        }
+    }
+}
---
+++ projects/d3p/IronySample/IronySample/Program.cs
@@ -0,0 +1,54 @@
+﻿using Irony.Parsing;
+using IronySamleGrammer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grammer = new IronySamleGrammer.GrammarDefinition();
+
+            Parser parser = new Parser(grammer);
+
+            string code = $@"
+service システムが利用できるか
+	resp
+        システム利用停止の状況に応じて、
+        指定のユーザがログインできるか判断する
+
+	methods
+		+ チェックする
+			args
+				: テナントコード
+				: 会社ID
+				対象ユーザID : ユーザID
+";
+
+            string code2 = $@"
+service システムが利用できるか
+";
+            string code3 = "require";
+
+            var parseTree = parser.Parse(code);
+
+            if (parseTree.Status == ParseTreeStatus.Parsed)
+            {
+                var classBlock = parseTree.Root;
+                var classModel = new ClassModel(classBlock);
+
+                parseTree.ToString();
+            } else
+            {
+                parseTree.ToString();
+            }
+
+
+        }
+    }
+}
