9f4e64d3b3e5f109435a02d47fb7c259ae8306c7
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-05-10 10:01:08 +0900

    carrot: 余計なコードを除去

--- projects/carrot/carrot.console/src/main/java/carrot/console/JankenConsole.java
+++ projects/carrot/carrot.console/src/main/java/carrot/console/JankenConsole.java
@@ -1,14 +1,7 @@
 package carrot.console;
 
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-
-import carrot.console.file.elimination.JsonSingleElimination;
-import carrot.game.player.JankenPlayer;
-import carrot.game.player.sample.AlwaysPaPlayer;
-import carrot.game.player.sample.CountingPlayer;
 import carrot.game.player.sample.PreviousHandPlayer;
+import carrot.game.player.sample.RandomPlayer;
 
 public class JankenConsole {
 
@@ -23,20 +16,9 @@ public class JankenConsole {
 		int numberOfRounds = 300;
 		
 		// 一対一の１ゲーム限り
-		//start(numberOfRounds);
-		
-		// 総当たり戦
-		//ConsoleRoundRobin.start(numberOfRounds);
-		
-		List<JankenPlayer> players = Arrays.asList(
-				new AlwaysPaPlayer(),
-				new CountingPlayer(),
-				new PreviousHandPlayer()
-				);
-		
-		JsonSingleElimination.start(
+		ConsoleOneGame.start(
 				numberOfRounds,
-				players,
-				Paths.get("F:\\biz\\Temp\\2021\\05\\work\\janken-result.json"));
+				new PreviousHandPlayer(),
+				new RandomPlayer());
 	}
 }
