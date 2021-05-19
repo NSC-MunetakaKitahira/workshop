ad77ccb6c444a6fae4a4ab84e66c7b59446e1e38
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-05-19 11:31:19 +0900

    kitahira

--- projects/carrot/carrot.game/src/main/java/carrot/game/player/kitahira/ExpectedValuePlayer.java
+++ projects/carrot/carrot.game/src/main/java/carrot/game/player/kitahira/ExpectedValuePlayer.java
@@ -34,12 +34,13 @@ public class ExpectedValuePlayer implements JankenPlayer {
 		
 		records.add(previousHand, currentGameStatus.previousOpponentHand);
 		
-		return previousHand = records.optimumHand();
+		return previousHand = records.optimumHand(currentGameStatus);
 	}
 	
 	static class Records {
 		
 		private Random random = new Random();
+		private boolean isYabaiMode = false;
 		
 		final List<Record> records = new ArrayList<>(Arrays.asList(
 				new Record(GU, WIN),
@@ -55,41 +56,16 @@ public class ExpectedValuePlayer implements JankenPlayer {
 		double expectedValueOf(JankenHand hand) {
 
 			double win = probability(hand, WIN) * hand.winnerGain;
-			double draw = probability(hand, DRAW) * 1;
-			double lose = probability(hand, LOSE) * hand.handToWin().winnerGain * -1;
+			double draw = probability(hand, DRAW) * 0.6;
+			double lose = probability(hand, LOSE) * hand.handToWin().winnerGain * -2;
 			return win + draw + lose;
 		}
 		
-		private List<Record> amplifiedRecords() {
-			
-			List<Record> results = new ArrayList<>();
-			
-			for (int i = records.size() - 1; i >= 0; i--) {
-				int count = records.size() - i;
-				Record record = records.get(i);
-				
-				if (count < 10) {
-					results.add(record);
-					results.add(record);
-					results.add(record);
-				}
-				
-				if (count < 30) {
-					results.add(record);
-					results.add(record);
-				}
-				
-				results.add(record);
-			}
-			
-			return results;
-		}
-		
-		JankenHand optimumHand() {
+		JankenHand optimumHand(SubjectiveMatchStatus status) {
 			
 			double rateGu, rateChoki, ratePa;
 			{
-				double gu = expectedValueOf(GU);
+				double gu = expectedValueOf(GU) * 0.5;
 				double choki = expectedValueOf(CHOKI);
 				double pa = expectedValueOf(PA);
 
@@ -103,6 +79,28 @@ public class ExpectedValuePlayer implements JankenPlayer {
 			double sum = rateGu + rateChoki + ratePa;
 			double rand = random.nextDouble() * sum;
 			
+			JankenHand hand = choiceHand(rateGu, ratePa, rand);
+			
+			int remainRounds = Math.max(1, status.maxRound - status.round);
+			if (status.round > 50 && status.ownScore + (4000 / remainRounds) < status.opponentScore) {
+				isYabaiMode = true;
+			}
+			
+			if (isYabaiMode) {
+				if (status.ownScore > status.opponentScore) {
+					isYabaiMode = false;
+				}
+			}
+			
+			if (isYabaiMode) {
+				System.out.print("  yaba  ");
+				return random.nextBoolean() ? hand : hand.handToLose();
+			}
+			
+			return hand;
+		}
+
+		private JankenHand choiceHand(double rateGu, double ratePa, double rand) {
 			if (rand < rateGu) {
 				return GU;
 			}
@@ -123,6 +121,37 @@ public class ExpectedValuePlayer implements JankenPlayer {
 					.count();
 			
 			return (double) count / (double) recordsTheHand.size();
+		}
+		
+		/**
+		 * 最近の実績を増幅して返す（新しい実績ほど重みが強いとみなす）
+		 * @return
+		 */
+		private List<Record> amplifiedRecords() {
+			
+			List<Record> results = new ArrayList<>();
+			
+			for (int i = records.size() - 1; i >= 0; i--) {
+				int count = records.size() - i;
+				Record record = records.get(i);
+				
+				if (count < 10) {
+					results.add(record);
+					results.add(record);
+					results.add(record);
+					results.add(record);
+				}
+				
+				if (count < 30) {
+					results.add(record);
+					results.add(record);
+					results.add(record);
+				}
+				
+				results.add(record);
+			}
+			
+			return results;
 		}
 	}
 
