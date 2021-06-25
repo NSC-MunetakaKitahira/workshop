cd5bef12325b4c28ff5a14e6b1c18ed1ef0986b4
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-06-25 09:29:28 +0900

    ビルドエラー修正

--- projects/carrot/carrot.web/src/main/java/carrot/web/Player.java
+++ projects/carrot/carrot.web/src/main/java/carrot/web/Player.java
@@ -2,21 +2,14 @@ package carrot.web;
 
 import static java.util.stream.Collectors.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import carrot.game.player.JankenPlayer;
 import carrot.game.player.Players;
-import carrot.game.player.sample.AlwaysPaPlayer;
-import carrot.game.player.sample.CountingPlayer;
-import carrot.game.player.sample.PreviousHandPlayer;
-import carrot.game.player.sample.RandomPlayer;
 
 /**
  * 参加可能なプレイヤーを定義する
@@ -24,19 +17,6 @@ import carrot.game.player.sample.RandomPlayer;
 @Produces(MediaType.APPLICATION_JSON)
 @Path("players")
 public class Player {
-
-	private static final List<JankenPlayer> PLAYERS = Arrays.asList(
-			new AlwaysPaPlayer(),
-			new CountingPlayer(),
-			new PreviousHandPlayer(),
-			new RandomPlayer()
-			);
-
-	public static Optional<JankenPlayer> findByName(String playerName) {
-		return PLAYERS.stream()
-				.filter(p -> p.getClass().getSimpleName().equals(playerName))
-				.findFirst();
-	}
 	
 	@GET
 	@Path("names")
